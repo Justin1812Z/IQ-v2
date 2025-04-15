25f9cc9029c12b9a793ddb9b490bebe930e3cd99
Justin Zitel <justin.l.zitel@gmail.com>
2025-04-15 14:26:16 -0400

    Im stupid but at least now I know why

--- Frontend/src/components/Flashcards/Flashcards.tsx
+++ Frontend/src/components/Flashcards/Flashcards.tsx
@@ -9,22 +9,62 @@ function Flashcards() {
     const [backendData, setBackendData] = useState<any>({});
     const [loading, setLoading] = useState(true);
     const params = useParams();
+    const [question, setQuestion] = useState("Loading first question");
+    const [answer, setAnswer] = useState("Loading first answer");
 
-    console.log("parameters: " + params.id)
+    
+    function updateCard2() {
+
+        //title.textContent = (cards[0].setName)
+    
+        
+    
+        console.log("setting question")
+        // questionElement.innerHTML = cards[currentIndex].question;
+        // answerElement.innerHTML = cards[currentIndex].answer;
+    
+        const showAnswerCheckbox = document.getElementById('showAnswerCheckbox') as HTMLInputElement;
+    
+    
+        if (currentIndex < cards.length) {
+            if ((invert && !flipped) || (!invert && flipped)) {
+                // questionElement.textContent = cards[currentIndex].question;
+                // answerElement.textContent = cards[currentIndex].answer;
+                setQuestion(cards[currentIndex].question);
+                setAnswer(cards[currentIndex].answer);
+                
+            } else if (invert) {
+                answerElement.textContent = cards[currentIndex].answer;
+                flipCard();
+                flashcard.addEventListener('transitionend', () => {
+                    questionElement.textContent = cards[currentIndex].question;
+                });
+            } else {
+                questionElement.textContent = cards[currentIndex].question;
+                flipCard();
+                flashcard.addEventListener('transitionend', () => {
+                    answerElement.textContent = cards[currentIndex].answer;
+                });
+            }
+        }
+    }
+   
 
     const fetchAPI = async () => {
         await axios
             .get("http://localhost:5000/flashcards/" + params.id)
             .then((response) => {
-                console.log("params: " + params);
                 setBackendData(response.data.cards);
                 cards = response.data.cards;
+                loadElements();
+
 
                 // You should NOT log backendData here, it's not updated yet
                 // // Log directly from the response
             })
             .then(() => {
                 setLoading(false)
+                updateCard2()
             })
     };
 
@@ -37,61 +77,74 @@ function Flashcards() {
     }
 
     return <>
-        <body>
-            <main>
-                <div id="container">
-                    <label>
-                        <h2 id="title">Error Loading Set Name</h2>
-                        <div>
-                            <strong>Start on back of card</strong>
-                            {/* <md-checkbox id="showAnswerCheckbox" onclick="updateCard() "></md-checkbox> */}
-                        </div>
-                    </label>
-
-                    <div className="flashcard" onClick={() => flipCard()}>
-                        <div className="card" id="flashcard">
-                            <div className="front" id="question">Click to see question/answer</div>
-                            <div className="back" id="answer">Click to see question/answer</div>
-                        </div>
+        <main>
+            <div id="container">
+                <label>
+                    <h2 id="title">Error Loading Set Name</h2>
+                    <div>
+                        <strong>Start on back of card</strong>
+                        {/* <md-checkbox id="showAnswerCheckbox" onclick="updateCard() "></md-checkbox> */}
                     </div>
+                </label>
 
-                    <div id="buttons">
-                        <button onClick={() => back()}>Back</button>
-                        <button onClick={() => next()}>Next</button>
+                <div className="flashcard" onClick={() => flipCard()}>
+                    <div className="card" id="flashcard">
+                        <div className="front" id="question">{question}</div>
+                        <div className="back" id="answer">{answer}</div>
                     </div>
                 </div>
 
+                <div id="buttons">
+                    <button onClick={() => back()}>Back</button>
+                    <button onClick={() => next()}>Next</button>
+                </div>
+            </div>
 
-            </main>
-        </body>
+
+        </main>
     </>
 }
 
 var cards: any[] = [];
 var title = document.getElementById('title') as HTMLTextAreaElement;
-//title.innerHTML = "testing innerhtml";
-console.log(cards);
 
-// console.log(cards);
 var currentIndex = 0;
+var questionElement = document.getElementById('question') as HTMLElement;
+var answerElement = document.getElementById('answer') as HTMLElement;
+var flashcard = document.getElementById('flashcard') as HTMLElement;
+
+var invert = false;
+var flipped = false;
+
+function loadElements() {
+    //var showAnswer = document.getElementById('showAnswerCheckbox').checked as HTMLInputElement;
+    questionElement = document.getElementById('question') as HTMLElement;
+    answerElement = document.getElementById('answer') as HTMLElement;
+    flashcard = document.getElementById('flashcard') as HTMLElement;
+
+    console.log("Element loaded")
+}
 
 function updateCard() {
-    //var showAnswer = document.getElementById('showAnswerCheckbox').checked as HTMLInputElement;
-    var questionElement = document.getElementById('question') as HTMLElement;
-    var answerElement = document.getElementById('answer') as HTMLElement;
-    var flashcard = document.getElementById('flashcard') as HTMLElement;
 
-    questionElement.innerHTML = cards[currentIndex].question;
-    answerElement.innerHTML = cards[currentIndex].answer;
+    //title.textContent = (cards[0].setName)
+
+    
+
+    console.log("setting question")
+    // questionElement.innerHTML = cards[currentIndex].question;
+    // answerElement.innerHTML = cards[currentIndex].answer;
 
     const showAnswerCheckbox = document.getElementById('showAnswerCheckbox') as HTMLInputElement;
 
 
     if (currentIndex < cards.length) {
-        if (showAnswerCheckbox?.checked && flashcard.classList.contains('flipped') || (!showAnswerCheckbox.checked && !flashcard.classList.contains('flipped'))) {
-            questionElement.textContent = cards[currentIndex].question;
-            answerElement.textContent = cards[currentIndex].answer;
-        } else if (showAnswerCheckbox.checked) {
+        if ((invert && !flipped) || (!invert && flipped)) {
+            // questionElement.textContent = cards[currentIndex].question;
+            // answerElement.textContent = cards[currentIndex].answer;
+   
+            
+        } else if (invert) {
             answerElement.textContent = cards[currentIndex].answer;
             flipCard();
             flashcard.addEventListener('transitionend', () => {
@@ -103,8 +156,6 @@ function updateCard() {
             flashcard.addEventListener('transitionend', () => {
                 answerElement.textContent = cards[currentIndex].answer;
             });
-
-
         }
     }
 }
@@ -112,6 +163,7 @@ function updateCard() {
 function flipCard() {
     const flashcard = document.getElementById('flashcard');
     flashcard?.classList.toggle('flipped');
+    flipped = !flipped;
 }
 
 function next() {
@@ -125,8 +177,6 @@ function back() {
 }
 
 
-document.addEventListener('DOMContentLoaded', function () {
-    updateCard();  // Initial display of card
-});
+
 
 export default Flashcards;
